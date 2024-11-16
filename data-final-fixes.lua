c1eff533031f972a72261ba767e82498f235861d
eugenikus8 <51211736+eugenikus8@users.noreply.github.com>
2024-11-16 19:59:48 +0200

    support SE [Update data-final-fixes.lua]

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -2,3 +2,10 @@ require("prototypes.fluids")
 require("prototypes.recipe_paste_update")
 
 require("prototypes.entity_update")
+
+if mods["space-age"] then
+     data.raw["assembling-machine"]["pneumatic-intake-filtered"].heating_energy = "30kW"
+     data.raw["furnace"]["pneumatic-intake"].heating_energy = "30kW"
+     data.raw["furnace"]["pneumatic-outtake"].heating_energy = "30kW"
+end 
+
