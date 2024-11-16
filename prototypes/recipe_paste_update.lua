98d924de3f54b8953a79cd568909a776e1ee46a7
eugenikus8 <51211736+eugenikus8@users.noreply.github.com>
2024-11-16 20:11:21 +0200

    Update recipe_paste_update.lua

--- prototypes/recipe_paste_update.lua
+++ prototypes/recipe_paste_update.lua
@@ -1,13 +1,13 @@
 
 for _, dbl in ipairs({data.raw["furnace"], data.raw["container"], data.raw["infinity-container"], data.raw["cargo-wagon"]}) do for name, entity in pairs(dbl) do
-  if entity.name ~= "pneumatic-end-point-intake-filtered" then
+  if entity.name ~= "pneumatic-intake-filtered" then
     if entity.additional_pastable_entities == nil then
       entity.additional_pastable_entities  = {}
     elseif type(entity.additional_pastable_entities) == "string" then
       entity.additional_pastable_entities = {entity.additional_pastable_entities}
     end
 
-    table.insert(entity.additional_pastable_entities, "pneumatic-end-point-intake-filtered")
+    table.insert(entity.additional_pastable_entities, "pneumatic-intake-filtered")
   end
 end end
 
