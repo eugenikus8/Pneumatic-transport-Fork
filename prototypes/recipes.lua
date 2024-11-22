50454802078526823fdb8ebd21140bbd8158c61c
eugenikus8 <51211736+eugenikus8@users.noreply.github.com>
2024-11-22 22:51:39 +0200

    add "Pneumatic outtake filtered"

--- prototypes/recipes.lua
+++ prototypes/recipes.lua
@@ -8,46 +8,64 @@ end
 
 
 data:extend({
-  {
-    type = "recipe",
-    name = "pneumatic-intake",
-    energy_required = 2.0,
-    enabled = false,
-    ingredients = {
+   {
+   type = "recipe",
+   name = "pneumatic-intake",
+   energy_required = 2.0,
+   enabled = false,
+   ingredients = {
       {type = "item", name = "iron-plate", amount = 2},
       {type = "item", name = "pipe", amount = 1},
       {type = "item", name = "iron-gear-wheel", amount = 2},
       {type = "item", name = boardname, amount = 1},
-    },
-    results = {{type = "item", name = "pneumatic-intake", amount = 1}},
-    category = "crafting",
-  },
-  {
-    type = "recipe",
-    name = "pneumatic-intake-filtered",
-    energy_required = 2.0,
-    enabled = false,
-    ingredients = {
-      {type = "item", name = "iron-plate", amount = 2},
-      {type = "item", name = "pipe", amount = 1},
-      {type = "item", name = "iron-gear-wheel", amount = 2},
-      {type = "item", name = boardname, amount = 1},
-    },
-    results = {{type = "item", name = "pneumatic-intake-filtered", amount = 1}},
-    category = "crafting",
-  },
-  {
-    type = "recipe",
-    name = "pneumatic-outtake",
-    energy_required = 2.0,
-    enabled = false,
-    ingredients = {
+   },
+   results = {{type = "item", name = "pneumatic-intake", amount = 1}},
+   category = "crafting",
+   },
+
+   {
+   type = "recipe",
+   name = "pneumatic-outtake",
+   energy_required = 2.0,
+   enabled = false,
+   ingredients = {
       { type = "item", name = "iron-plate", amount = 2 },
       { type = "item", name = "pipe", amount = 1 },
       { type = "item", name = "iron-gear-wheel", amount = 2 },
       { type = "item", name = boardname, amount = 1 },
-    },
-    results = {{type = "item", name = "pneumatic-outtake", amount = 1}},
-    category = "crafting",
-  },
+   },
+   results = {{type = "item", name = "pneumatic-outtake", amount = 1}},
+   category = "crafting",
+   },
+
+   {
+   type = "recipe",
+   name = "pneumatic-intake-filtered",
+   energy_required = 2.0,
+   enabled = false,
+   ingredients = {
+      {type = "item", name = "iron-plate", amount = 2},
+      {type = "item", name = "pipe", amount = 1},
+      {type = "item", name = "iron-gear-wheel", amount = 2},
+      {type = "item", name = boardname, amount = 1},
+   },
+   results = {{type = "item", name = "pneumatic-intake-filtered", amount = 1}},
+   category = "crafting",
+   },
+
+   {
+   type = "recipe",
+   name = "pneumatic-outtake-filtered",
+   energy_required = 2.0,
+   enabled = false,
+   ingredients = {
+      {type = "item", name = "iron-plate", amount = 2},
+      {type = "item", name = "pipe", amount = 1},
+      {type = "item", name = "iron-gear-wheel", amount = 2},
+      {type = "item", name = boardname, amount = 1},
+   },
+   results = {{type = "item", name = "pneumatic-outtake-filtered", amount = 1}},
+   category = "crafting",
+   },
+
 })
