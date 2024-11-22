42269337f0f9360edcef9f71c51722b98820ed80
eugenikus8 <51211736+eugenikus8@users.noreply.github.com>
2024-11-22 22:46:50 +0200

    Update data-final-fixes.lua

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -4,8 +4,9 @@ require("prototypes.recipe_paste_update")
 require("prototypes.entity_update")
 
 if mods["space-age"] then
-     data.raw["assembling-machine"]["pneumatic-intake-filtered"].heating_energy = "30kW"
      data.raw["furnace"]["pneumatic-intake"].heating_energy = "30kW"
      data.raw["furnace"]["pneumatic-outtake"].heating_energy = "30kW"
+     data.raw["assembling-machine"]["pneumatic-intake-filtered"].heating_energy = "30kW"
+     data.raw["assembling-machine"]["pneumatic-outtake-filtered"].heating_energy = "30kW"
 end 
 
