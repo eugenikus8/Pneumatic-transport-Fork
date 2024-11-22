f12e8ea050e6d90dcf3c90f1f65a53c62c4f3f0a
eugenikus8 <51211736+eugenikus8@users.noreply.github.com>
2024-11-22 22:53:39 +0200

    add "Pneumatic outtake filtered"

--- prototypes/items.lua
+++ prototypes/items.lua
@@ -1,34 +1,49 @@
 local icons_path = "__pneumatic-transport__/graphics/icon/entities/"
 
 data:extend({
-  {
-    type = "item",
-    name = "pneumatic-intake-filtered",
-    icon = icons_path .. "intake-filtered.png",
-    icon_size = 32,
-    place_result = "pneumatic-intake-filtered",
-    subgroup = "inserter",
-    order = "p[pneumatic]-a3",
-    stack_size = 50,
-  },
-  {
-    type = "item",
-    name = "pneumatic-intake",
-    icon = icons_path .. "intake.png",
-    icon_size = 32,
-    place_result = "pneumatic-intake",
-    subgroup = "inserter",
-    order = "p[pneumatic]-a2",
-    stack_size = 50,
-  },
-  {
-    type = "item",
-    name = "pneumatic-outtake",
-    icon = icons_path .. "outtake.png",
-    icon_size = 32,
-    place_result = "pneumatic-outtake",
-    subgroup = "inserter",
-    order = "p[pneumatic]-a1",
-    stack_size = 50,
-  },
+
+   {
+      type = "item",
+      name = "pneumatic-intake",
+      icon = icons_path .. "intake.png",
+      icon_size = 32,
+      place_result = "pneumatic-intake",
+      subgroup = "inserter",
+      order = "p[pneumatic]-a1",
+      stack_size = 50,
+   },
+
+   {
+      type = "item",
+      name = "pneumatic-outtake",
+      icon = icons_path .. "outtake.png",
+      icon_size = 32,
+      place_result = "pneumatic-outtake",
+      subgroup = "inserter",
+      order = "p[pneumatic]-a2",
+      stack_size = 50,
+   },
+
+   {
+      type = "item",
+      name = "pneumatic-intake-filtered",
+      icon = icons_path .. "intake-filtered.png",
+      icon_size = 32,
+      place_result = "pneumatic-intake-filtered",
+      subgroup = "inserter",
+      order = "p[pneumatic]-a3",
+      stack_size = 50,
+   },
+
+   {
+      type = "item",
+      name = "pneumatic-outtake-filtered",
+      icon = icons_path .. "outtake-filtered.png",
+      icon_size = 32,
+      place_result = "pneumatic-outtake-filtered",
+      subgroup = "inserter",
+      order = "p[pneumatic]-a4",
+      stack_size = 50,
+   },
+
 })
