dbc470a37b71b0ce12f4a8cef0dcaf4be51023eb
eugenikus8 <51211736+eugenikus8@users.noreply.github.com>
2024-11-22 22:48:23 +0200

    add "Pneumatic outtake filtered"

--- prototypes/recipe_paste_update.lua
+++ prototypes/recipe_paste_update.lua
@@ -1,13 +1,13 @@
-
-for _, dbl in ipairs({data.raw["furnace"], data.raw["container"], data.raw["infinity-container"], data.raw["cargo-wagon"]}) do for name, entity in pairs(dbl) do
-  if entity.name ~= "pneumatic-intake-filtered" then
-    if entity.additional_pastable_entities == nil then
-      entity.additional_pastable_entities  = {}
-    elseif type(entity.additional_pastable_entities) == "string" then
-      entity.additional_pastable_entities = {entity.additional_pastable_entities}
-    end
-
-    table.insert(entity.additional_pastable_entities, "pneumatic-intake-filtered")
-  end
-end end
-
+for _, dbl in ipairs({data.raw["furnace"], data.raw["container"], data.raw["infinity-container"], data.raw["cargo-wagon"]}) do
+   for name, entity in pairs(dbl) do
+      if entity.name ~= "pneumatic-intake-filtered" and entity.name ~= "pneumatic-outtake-filtered" then
+         if entity.additional_pastable_entities == nil then
+            entity.additional_pastable_entities = {}
+         elseif type(entity.additional_pastable_entities) == "string" then
+            entity.additional_pastable_entities = {entity.additional_pastable_entities}
+         end
+         table.insert(entity.additional_pastable_entities, "pneumatic-intake-filtered")
+         table.insert(entity.additional_pastable_entities, "pneumatic-outtake-filtered")
+      end
+   end
+end
